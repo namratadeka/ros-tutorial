3c1c676f8b378753a818f2509ff664e2c809f7a8
Namrata Deka <namratadeka5@gmail.com>
2020-10-21 12:18:59 +0530

    remove urdf repos

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,6 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
-subdirs("urdf_sim_tutorial")
 subdirs("beginner_tutorials")
-subdirs("urdf_tutorial")
